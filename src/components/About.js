89a140bf498c4caa63257b0511cfc63b13968c33
vlmnst <vale-mansueto@hotmail.com>
2022-09-05 15:57:22 -0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -4,116 +4,116 @@ import { motion } from "framer-motion";
 import "./styles/About.css";
 
 function BoxText({ text, drop, arrayToRender }) {
-
   return (
     <div>
-        {
-            arrayToRender.includes(text) ?
-            (
-                <motion.div
-                className="About-box"
-                initial={{ y: 0 }}
-                animate={
-                  drop
-                    ? 
-                    {
-                        y: 150,
-                        // x: Math.random() * -500,
-                        transition: { duration: Math.random() * 2 },
-                        color: "#1b197e"
-                      }
-                    : {
-                        y: 0,
-                        x: 0,
-                        transition: { duration: Math.random() * 2 },
-                      }
+      {arrayToRender.includes(text) ? (
+        <motion.div
+          className="About-box"
+          initial={{ y: 0 }}
+          animate={
+            drop
+              ? {
+                  y: 150,
+                  transition: { duration: Math.random() * 2 },
+                  opacity: 0,
                 }
-                drag
-                dragConstraints={{
-                  top: -300,
-                  left: -600,
-                  right: 500,
-                  bottom: 480,
-                }}
-              >
-                <p className="About-text"> {text} </p>
-              </motion.div>
-            ) : (
-               
-                <motion.div
-                className="About-box"
-                initial={{ y: 0 }}
-                drag
-                dragConstraints={{
-                  top: -300,
-                  left: -600,
-                  right: 500,
-                  bottom: 480,
-                }}
-              >
-                <p className="About-text"> {text} </p>
-              </motion.div>
-            )
-        }
-     
+              : {
+                  y: 0,
+                  x: 0,
+                  transition: { duration: Math.random() * 2 },
+                }
+          }
+          drag
+          dragConstraints={{
+            top: -300,
+            left: -600,
+            right: 500,
+            bottom: 480,
+          }}
+        >
+          <p className="About-text"> {text} </p>
+        </motion.div>
+      ) : (
+        <motion.div
+          className="About-box"
+          initial={{ y: 0 }}
+          drag
+          dragConstraints={{
+            top: -300,
+            left: -600,
+            right: 500,
+            bottom: 480,
+          }}
+        >
+          <p className="About-text"> {text} </p>
+        </motion.div>
+      )}
     </div>
   );
 }
 
 export default function About() {
   let stringAbout =
-    "Somos un equipo que crea aplicaciones modernas e intuitivas usando las tecnologías más demandadas del mercado. En Bue! Desarrollamos tu idea. Buenas apps, buenos momentos.",
+      "Somos un equipo que crea aplicaciones modernas e intuitivas usando las tecnologías más demandadas del mercado. En Bue! Desarrollamos tu idea. Buenas apps, buenos momentos.",
     stringAbout1 = "tu idea. crea momentos.",
     stringAbout2 = "Somos más que un equipo",
     stringAbout3 = "Desarrollamos más que aplicaciones",
-    stringAbout4 = "crea apps, intuitivas"
+    stringAbout4 = "crea apps, intuitivas";
   let arrayAbout = stringAbout.split(" ");
   let arrayAbout1 = stringAbout1.split(" ");
   let arrayAbout2 = stringAbout2.split(" ");
   let arrayAbout3 = stringAbout3.split(" ");
   let arrayAbout4 = stringAbout4.split(" ");
 
-  let allPhrases = [ arrayAbout1, arrayAbout2, arrayAbout3, arrayAbout4 ]
- 
-  let [count, setCount] = useState(0)
-  const [arrayToRender, setArrayToRender] = useState(allPhrases[count])
+  let allPhrases = [arrayAbout1, arrayAbout2, arrayAbout3, arrayAbout4];
+
+  let [count, setCount] = useState(0);
+  const [arrayToRender, setArrayToRender] = useState(allPhrases[count]);
   const [drop, setDrop] = useState(false);
 
   const handleClick = (e) => {
-    e.preventDefault()
-    if (!drop){
-       setDrop(!drop) 
+    e.preventDefault();
+    if (!drop) {
+      setDrop(!drop);
     }
-    if(drop){
-      if(count < 4){
-      setCount(++count)  
-    }
-    if(count === 3){
-        setCount(0)  
+    if (drop) {
+      if (count < 4) {
+        setCount(++count);
       }
-    setArrayToRender(allPhrases[count])    
-    setDrop(!drop) 
+      if (count === 3) {
+        setCount(0);
+      }
+      setArrayToRender(allPhrases[count]);
+      setDrop(!drop);
     }
-  }
-  let random = true
-  
+  };
+  let random = true;
+
   return (
     <motion.div
       className="About"
       initial={{ scale: 0, transition: { duration: 0.1 } }}
       animate={{ x: 0, scale: 1, transition: { duration: 0.7 } }}
     >
-        <button onClick={(e) => handleClick(e)}>
-      <motion.div className={"About-dashboard"}>
-        {arrayAbout?.map((world, indice) => {
-          random = !random
-          return(
-            <BoxText text={world} key={indice} drop={drop} arrayToRender={arrayToRender} />
-          )
-        })}
-      </motion.div>
+      <button onClick={(e) => handleClick(e)}>
+        <motion.div className={"About-dashboard"}>
+          {arrayAbout?.map((world, indice) => {
+            random = !random;
+            return (
+              <BoxText
+                text={world}
+                key={indice}
+                drop={drop}
+                arrayToRender={arrayToRender}
+              />
+            );
+          })}
+          <motion.div className={"About-dashboard-text"} >{drop? arrayToRender : null}</motion.div>
+        </motion.div>
       </button>
-      <Link to="/">Back</Link>
+      <Link className="Back" to="/">
+        Back
+      </Link>
     </motion.div>
   );
 }
